3fed91039ea3f48024e51561fa84fbe76a74b3ba
timmtonga <timmtonga@gmail.com>
2016-03-09 13:29:45 -0500

    Added date picker library

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( jquery-1.12.0.min.js )
-Rails.application.config.assets.precompile += %w( dataTables.bootstrap4.min.js )
-Rails.application.config.assets.precompile += %w( jquery.dataTables.min.js )
+
