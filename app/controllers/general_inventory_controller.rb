1b811408d7125edf8290b6b63aa0fbf48832f1e7
timmtonga <timmtonga@gmail.com>
2016-03-10 19:11:41 -0500

    Changed expiry date to only capture month and year

--- app/controllers/general_inventory_controller.rb
+++ app/controllers/general_inventory_controller.rb
@@ -15,18 +15,24 @@ class GeneralInventoryController < ApplicationController
 
     # Create a new record for general inventory
 
-    new_stock_entry = GeneralInventory.new
-    new_stock_entry.lot_number = params[:general_inventory][:lot_number]
-    new_stock_entry.expiration_date = params[:general_inventory][:expiration_date]
-    new_stock_entry.received_quantity = params[:general_inventory][:quantity_received]
-    new_stock_entry.rxaui = Rxnconso.where("STR = ?", params[:general_inventory][:item]).first.RXAUI rescue nil
-
-      GeneralInventory.transaction do
-        new_stock_entry.save!
-      end
+    @new_stock_entry = GeneralInventory.new
+    @new_stock_entry.lot_number = params[:general_inventory][:lot_number].upcase
+    @new_stock_entry.expiration_date = params[:general_inventory][:expiration_date].to_date rescue nil
+    @new_stock_entry.received_quantity = params[:general_inventory][:received_quantity]
+    @new_stock_entry.rxaui = Rxnconso.where("STR = ?", params[:general_inventory][:item]).first.RXAUI rescue nil
 
 
-    render :text => new_stock_entry.errors
+    GeneralInventory.transaction do
+      @new_stock_entry.save
+    end
+
+    flash[:errors] = @new_stock_entry.errors
+
+
+    redirect_to "/general_inventory"
+
+
+
   end
 
 end
