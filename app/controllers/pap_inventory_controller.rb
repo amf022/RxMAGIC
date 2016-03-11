f31e0df1dfdef9a1e21ca95de3fb88c9701d875d
timmtonga <timmtonga@gmail.com>
2016-03-11 17:31:19 -0500

    Added deleting for pmap meds

--- app/controllers/pap_inventory_controller.rb
+++ app/controllers/pap_inventory_controller.rb
@@ -1,7 +1,7 @@
 class PapInventoryController < ApplicationController
 
   def index
-    @inventory = PapInventory.where("current_quantity > 0").order(date_received: :asc)
+    @inventory = PapInventory.where("current_quantity > 0 and voided = ?", false).order(date_received: :asc)
   end
 
   def show
@@ -10,6 +10,12 @@ class PapInventoryController < ApplicationController
   def edit
   end
 
-  def delete
+  def destroy
+    #Delete an item from general inventory
+
+    item = PapInventory.find_by_pap_identifier(params[:id])
+    item.update_attributes(:voided => true)
+    flash[:success] = " #{item.drug_name} #{item.lot_number} was successfully deleted."
+    redirect_to "/pap_inventory"
   end
 end
