759e3f6234873b75817bd4dacf6616a077257179
timmtonga <timmtonga@gmail.com>
2016-04-06 17:58:50 -0400

    Finalized general inventory functions

--- app/controllers/general_inventory_controller.rb
+++ app/controllers/general_inventory_controller.rb
@@ -24,7 +24,18 @@ class GeneralInventoryController < ApplicationController
 
     # Create a new record for general inventory
 
-    @new_stock_entry = GeneralInventory.new
+    if params[:general_inventory][:inventory_id].blank?
+      @new_stock_entry = GeneralInventory.new
+    else
+      @new_stock_entry = GeneralInventory.find(params[:general_inventory][:inventory_id])
+      if (@new_stock_entry.received_quantity - @new_stock_entry.current_quantity) > params[:general_inventory][:received_quantity].to_f
+        flash[:errors][:counts] = [" The number items that have already been dispensed from this bottle is more than the received quantity."]
+      else
+          @new_stock_entry.current_quantity = params[:general_inventory][:received_quantity].to_f - (@new_stock_entry.received_quantity - @new_stock_entry.current_quantity)
+      end
+
+    end
+
     @new_stock_entry.lot_number = params[:general_inventory][:lot_number].upcase
     @new_stock_entry.expiration_date = params[:general_inventory][:expiration_date].to_date rescue nil
     @new_stock_entry.received_quantity = params[:general_inventory][:received_quantity]
@@ -37,7 +48,12 @@ class GeneralInventoryController < ApplicationController
 
     if @new_stock_entry.errors.blank?
       #print barcode for new bottles
-      print_and_redirect("/print_bottle_barcode/#{@new_stock_entry.gn_identifier}", "/general_inventory")
+      if params[:general_inventory][:inventory_id].blank?
+        print_and_redirect("/print_bottle_barcode/#{@new_stock_entry.gn_identifier}", "/general_inventory")
+      else
+        flash[:success] = "#{params[:general_inventory][:item]} (Lot #: #{@new_stock_entry.lot_number}) was successfully updated."
+        redirect_to "/general_inventory"
+      end
     else
       flash[:errors] = @new_stock_entry.errors
       redirect_to "/general_inventory"
