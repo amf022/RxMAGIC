b9c91ccc7004f657c0cc77c1dcdba079fd055b1f
timmtonga <timmtonga@gmail.com>
2016-05-05 19:06:45 -0400

    Improved editing of general items

--- app/controllers/general_inventory_controller.rb
+++ app/controllers/general_inventory_controller.rb
@@ -59,10 +59,13 @@ class GeneralInventoryController < ApplicationController
 
     if params[:general_inventory][:inventory_id].blank?
       @new_stock_entry = GeneralInventory.new
+      @new_stock_entry.rxaui = Rxnconso.where("STR = ?", params[:general_inventory][:item]).first.RXAUI rescue nil
+      name = params[:general_inventory][:item]
     else
       @new_stock_entry = GeneralInventory.find(params[:general_inventory][:inventory_id])
+      name = @new_stock_entry.drug_name
       if (@new_stock_entry.received_quantity - @new_stock_entry.current_quantity) > params[:general_inventory][:received_quantity].to_f
-        flash[:errors][:counts] = [" The number items that have already been dispensed from this bottle is more than the received quantity."]
+        flash[:errors][:counts] = [" The number of items that have already been dispensed from this bottle is more than the received quantity."]
       else
           @new_stock_entry.current_quantity = params[:general_inventory][:received_quantity].to_f - (@new_stock_entry.received_quantity - @new_stock_entry.current_quantity)
       end
@@ -72,8 +75,6 @@ class GeneralInventoryController < ApplicationController
     @new_stock_entry.lot_number = params[:general_inventory][:lot_number].upcase
     @new_stock_entry.expiration_date = params[:general_inventory][:expiration_date].to_date rescue nil
     @new_stock_entry.received_quantity = params[:general_inventory][:received_quantity]
-    @new_stock_entry.rxaui = Rxnconso.where("STR = ?", params[:general_inventory][:item]).first.RXAUI rescue nil
-
 
     GeneralInventory.transaction do
       @new_stock_entry.save
@@ -84,7 +85,7 @@ class GeneralInventoryController < ApplicationController
       if params[:general_inventory][:inventory_id].blank?
         print_and_redirect("/print_bottle_barcode/#{@new_stock_entry.gn_identifier}", "/general_inventory")
       else
-        flash[:success] = "#{params[:general_inventory][:item]} (Lot #: #{@new_stock_entry.lot_number}) was successfully updated."
+        flash[:success] = "#{name} (Lot #: #{@new_stock_entry.lot_number}) was successfully updated."
         redirect_to "/general_inventory"
       end
     else
