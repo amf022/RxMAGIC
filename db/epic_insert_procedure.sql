ca4b5af79e99405589920230933a964aade1e1f7
timmtonga <timmtonga@gmail.com>
2016-06-05 18:10:51 -0400

    Made adjustments to the epic insert procedure

--- db/epic_insert_procedure.sql
+++ db/epic_insert_procedure.sql
@@ -22,6 +22,7 @@ CREATE PROCEDURE `proc_insert_epic_prescriptions`(
 )
 BEGIN
 
+  START TRANSACTION;	
 	UPDATE patients SET first_name = patient_fname, last_name = patient_lname, gender = patient_gender, birthdate = STR_TO_DATE(patient_birthdate, '%Y%m%d'), address = patient_address, city = patient_city, state = patient_state, zip = patient_state, phone = patient_phone, voided= false,updated_at = now() WHERE epic_id= patient_epic_id;
 	
 	IF (ROW_COUNT() = 0) THEN		
@@ -36,15 +37,26 @@ BEGIN
 	END IF;
 
 	SET @drug_id = (SELECT RXAUI FROM  RXNSAT  WHERE ATV = ndc_code LIMIT 1);
+	SET @drug_name = (SELECT STR FROM RXNCONSO WHERE RXAUI = @drug_id LIMIT 1);
 	SET @patient_id = (SELECT patient_id FROM patients WHERE epic_id= patient_epic_id LIMIT 1);
 	SET @provider_id = (SELECT provider_id FROM providers WHERE first_name = provider_fname AND last_name = provider_lname LIMIT 1);
 
-	UPDATE prescriptions SET patient_id = @patient_id, rxaui = @drug_id, date_prescribed = STR_TO_DATE(rx_date_prescribed, '%Y%m%d %h%i%s'), quantity = rx_quantity, directions = rx_directions, provider_id = @provider_id, voided = FALSE, updated_at = now() WHERE rxaui = @drug_id AND patient_id = @patient_id AND DATE(date_prescribed) = STR_TO_DATE(rx_date_prescribed, '%Y%m%d');
+  IF (@drug_id IS NULL) THEN
+    COMMIT;
+  ELSE
+	  UPDATE prescriptions SET patient_id = @patient_id, rxaui = @drug_id, date_prescribed = COALESCE(STR_TO_DATE(rx_date_prescribed, '%Y%m%d %h%i%s'), now()), quantity = COALESCE(rx_quantity,0), directions = rx_directions, provider_id = @provider_id, voided = FALSE, updated_at = now() WHERE rxaui = @drug_id AND patient_id = @patient_id AND DATE(date_prescribed) = COALESCE(STR_TO_DATE(rx_date_prescribed, '%Y%m%d'), DATE(now()));
+	
+    IF (ROW_COUNT() = 0) THEN
+		  INSERT INTO prescriptions (patient_id, rxaui,date_prescribed, quantity, directions, provider_id, voided, created_at, updated_at) VALUES ( @patient_id,@drug_id, COALESCE(STR_TO_DATE(rx_date_prescribed, '%Y%m%d %h%i%s'), now()), COALESCE(rx_quantity,0), rx_directions, @provider_id, FALSE, now(), now());
 
-	IF (ROW_COUNT() = 0) THEN
-		INSERT INTO prescriptions (patient_id, rxaui,date_prescribed, quantity, directions, provider_id, voided, created_at, updated_at) VALUES ( @patient_id,@drug_id, COALESCE(STR_TO_DATE(rx_date_prescribed, '%Y%m%d %h%i%s'), now()), rx_quantity, rx_directions, @provider_id, FALSE, now(), now());
-	END IF;
+      SET @prescription_id = (SELECT rx_id FROM prescriptions WHERE patient_id = @patient_id AND rxaui = @drug_id AND DATE(date_prescribed) = COALESCE(STR_TO_DATE(rx_date_prescribed, '%Y%m%d'), DATE(now())) ORDER BY date_prescribed DESC LIMIT 1);
+      
+      SET @msg = CONCAT(rx_quantity, " of ", @drug_name, " for ",  patient_fname," ", patient_lname);
 
+      INSERT INTO news (message, refers_to, news_type, created_at, updated_at) VALUES (@msg,@prescription_id,"new prescription",now(),now());
+	  END IF;
+    COMMIT;
+  END IF;
 
 END$$
 
