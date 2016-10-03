1cadde34e05c81991eb61dfe51e4ed37c7b3e3c7
timmtonga <timmtonga@gmail.com>
2016-10-03 14:44:15 -0400

    changed line due to dependencies

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -117,8 +117,8 @@ class UserController < ApplicationController
       end
 
       if !session[:user_token].blank?
-        logger.info "#{current_user.username} logged in at #{Time.now}"
         session[:user] = params[:user][:username]
+        logger.info "#{current_user.username} logged in at #{Time.now}"
         user_role = User.find_by_username(params[:user][:username])
         if user_role.blank?
           redirect_to "/new_user_role"
