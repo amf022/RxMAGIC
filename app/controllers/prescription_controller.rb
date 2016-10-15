4631d58d3b5185252b9ecc5f8472257db015c309
Timothy Mtonga <timmtonga@gmail.com>
2016-10-15 12:38:20 -0400

    Changed printing column width for stock bottles and added voidding of Rx news when a Rx is voided

--- app/controllers/prescription_controller.rb
+++ app/controllers/prescription_controller.rb
@@ -25,6 +25,16 @@ class PrescriptionController < ApplicationController
 
     prescription = Prescription.find(params[:id])
     prescription.update_attributes(:voided => true)
+    if prescription.errors.blank?
+      news = News.where("refers_to = ? AND resolved = ?", params[:id], false).first
+      unless news.blank?
+        news.resolved = true
+        news.resolution = "Rx was cancelled"
+        news.date_resolved= Date.today
+        news.save
+        logger.info "Prescription #{params[:id]} was voided by #{current_user.username}"
+      end
+    end
     redirect_to "/prescription"
   end
 
